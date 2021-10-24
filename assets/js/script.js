b73475cfb878b4187aa250eb807be21471342b45
Jess Varghese <jessvarghese@gmail.com>
2021-10-23 22:01:20 -0400

    updated questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,38 @@
-
 var startButton = document.getElementById("start-btn");
 var countDown = document.getElementById("timer");
-var answerButton = document.getElementById("answer");
+var qTwo = document.getElementById("answer-1");
+var qThree = document.getElementById("answer-2");
+var qFour = document.getElementById("answer-3");
+var qFive = document.getElementById("answer-4");
+
+var wrong = document.getElementsByClassName("wrong");
+var correct = document.getElementsByClassName("correct");
+
 
 
 
 startButton.addEventListener("click", startTimer);
 startButton.addEventListener("click", questionOne);
+qTwo.addEventListener("click", questionTwo);
+qThree.addEventListener("click", questionThree);
+qFour.addEventListener("click", questionFour);
+qFive.addEventListener("click", questionFive);
+
+
+
 
 
 //Function for when the user clicks the button to start the quiz
 let time =90;
-let timerMinutes = time * 60 * 60;
+let timerMinutes = time * 60;
 let timer = timerMinutes /60;
 
 function startTimer() {
   startButton
   let timer = setInterval(() => {
     countDown.innerHTML = "Timer: " + time;
-    time--;
-    if (time<= 0) {
+    // time--;
+    if (time<= -1) {
       clearInterval(timer);
   }
     
@@ -34,7 +47,53 @@ function questionOne() {
   
   document.querySelector(".intro-container").style.display="none"
 
-}
+  if (wrong) {
+    return (timer - 10);
+  }
+
+  if (qTwo===correct) {
+    return (timer + 5);
+  }
+
+};
+    
+
+
+function questionTwo() {
+  
+  document.querySelector(".question-2").style.display="block"
+  document.querySelector(".question-1").style.display="none"
+};
+
+
+
+function questionThree() {
+  
+  document.querySelector(".question-3").style.display="block"
+  document.querySelector(".question-2").style.display="none"
+};
+
+
+
+function questionFour() {
+  
+  document.querySelector(".question-4").style.display="block"
+  document.querySelector(".question-3").style.display="none"
+};
+
+
+
+function questionFive() {
+  
+  document.querySelector(".question-5").style.display="block"
+  document.querySelector(".question-4").style.display="none"
+
+};
+
+// function endGame() {
+
+// }
+
 
 
 
@@ -42,3 +101,5 @@ function questionOne() {
 // 1.	When a user answers each question they will be alerted if the answer is correct or not. Correct points will add points while incorrect answers will reduce the timer by 10 secs.
 // 2.	When the player completes the quiz they are prompted with adding their name to the scoreboard and seeing a list of high scores.
 // 3.	The player can click on “view high scores“to see how they compare to other players
+
+
